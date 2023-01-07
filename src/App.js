42c98178cebd854c41ff47eb4b50afb3762560c4
GRAF5 <23andrei12@gmail.com>
2023-01-07 14:36:45 +0200

    fix cs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
-import logo from './logo.svg';
 import './App.css';
-import Button from './components/button/button';
-import InputField from './components/input-field/input-field';
 
 function App() {
   return (
